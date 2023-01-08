a79a26f1da245792386b11c6781311ae4c45a4ee
Thibault HULAUX <thibault.hulaux@gmail.com>
2023-01-08 20:21:53 +0100

    Edit git scripts

--- scripts/git/git_history-prune.sh
+++ scripts/git/git_history-prune.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env sh
-#
+# shellcheck disable=SC2155
 #
 
 set -o errexit
-set -o nounset
 
 # ------------------------------------------------------------------- Globals -
 readonly PROGNAME="$(basename "$(readlink -f "${0}")")"
 readonly WORKINGDIR="$(pwd)"
 
-readonly GITUSERID="thibaulthulaux"
-readonly GITREPOID="$(basename "${WORKINGDIR}").git"
-
 # ----------------------------------------------------------------- Functions -
 eexit() { printf "%s\n" "${PROGNAME}: Exit: ${*}" 1>&2; exit 1; }
-info() { printf "%s\n" "${PROGNAME}: ${*}" >&2; }
+info() { printf "%s\n" "${PROGNAME}: ${*}" >&1; }
 
 user_confirm() {
-  printf "%s" "${PROGNAME}: ${*} (y/n): " >&2; read -r reply
+  printf "%s" "${PROGNAME}: ${*} (y/n): " >&1; read -r reply
   case "${reply}" in
     y|Y ) return 0;;
     * ) return 1;;
--- scripts/git/git_remote-set-origin.sh
+++ scripts/git/git_remote-set-origin.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
-#
+# shellcheck disable=SC2155
 #
 
 set -o errexit
-set -o nounset
 
 # ------------------------------------------------------------------- Globals -
 readonly PROGNAME="$(basename "$(readlink -f "${0}")")"
@@ -14,41 +13,36 @@ readonly GITREPOID="$(basename "${WORKINGDIR}").git"
 
 # ----------------------------------------------------------------- Functions -
 eexit() { printf "%s\n" "${PROGNAME}: Exit: ${*}" 1>&2; exit 1; }
-info() { printf "%s\n" "${PROGNAME}: ${*}" >&2; }
+info() { printf "%s\n" "${PROGNAME}: ${*}" >&1; }
 
 user_confirm() {
-  printf "%s" "${PROGNAME}: ${*} (y/n): " >&2; read -r reply
+  printf "%s" "${PROGNAME}: ${*} (y/n): " >&1; read -r reply
   case "${reply}" in
     y|Y ) return 0;;
     * ) return 1;;
   esac
 }
 
-# create_remote_repository() {
-# curl -u "${GITUSERID}" "https://api.github.com/user/repos" \
-#     -d "{\"name\": \"${WORKINGDIR##*/}\", \"description\": \"${2}\", \"private\": true, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
-# }
-
 # ---------------------------------------------------------------------- Main -
 main() {
   [ -d "${WORKINGDIR}/.git" ] \
     || eexit "Not a git repository, run 'git init' first"
 
-  user_confirm "Setup remote 'all' ?" \
+  user_confirm "Setup remote 'origin' ?" \
     || eexit "User interrupted"
 
-  info "Add github as remote 'all'"
+  info "Add github as remote 'origin'"
   git remote add \
-    all "git@github.com:${GITUSERID}/${GITREPOID}"
+    origin "git@github.com:${GITUSERID}/${GITREPOID}"
 
-  info "Re-register github as 'all' push URL"
+  info "Re-register github as 'origin' push URL"
   git remote set-url --add --push \
-    all "git@github.com:${GITUSERID}/${GITREPOID}"
+    origin "git@github.com:${GITUSERID}/${GITREPOID}"
 
-  info "Add gitlab as 'all' push URL"
+  info "Add gitlab as 'origin' push URL"
   git remote set-url --add --push \
-    all "git@gitlab.com:${GITUSERID}/${GITREPOID}"
-    
+    origin "git@gitlab.com:${GITUSERID}/${GITREPOID}"
+
   info "Done"
 }
 
